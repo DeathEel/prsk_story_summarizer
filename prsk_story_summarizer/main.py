6ba3d19d62aa375c9b791d90ad57baac64600c34
kdn765 <kdn765@gmail.com>
2025-06-05 01:33:07 -0700

    <feat>(main): Create sample app flow.
    This documents what modules must be created to develop the base app features.

---
+++ prsk_story_summarizer/main.py
@@ -0,0 +1,24 @@
+from db import init_db
+from scraper import get_stories, get_chapters
+from summarizer import summarize_chapter
+from menu import display_story_menu, display_chapter_menu
+
+def main():
+    init_db()
+
+    user_input = 1
+    while user_input != 0:
+        stories = get_stories()
+        selected_story = display_story_menu(stories)
+
+        chapters = get_chapters(selected_story['url'])
+        selected_chapter = display_chapter_menu(chapters)
+
+        summary = summarize_chapter(selected_chapter['text'])
+
+        print(summary)
+
+        user_input = int(input("\nChoose 0 to quit. Choose 1 to continue."))
+
+if __name__ == "__main__":
+    main()
