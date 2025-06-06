8ce4287433bf0a3771d9d72f52b2b52f1e2457d4
kdn765 <kdn765@gmail.com>
2025-06-06 05:36:01 -0700

    <chore>(main): Add functionality to support the other modules changing.

--- prsk_story_summarizer/main.py
+++ prsk_story_summarizer/main.py
@@ -1,10 +1,13 @@
-from db import init_db, get_or_scrape_stories, get_or_scrape_chapters
-from scraper import scrape_stories()
+from gpt4all import GPT4All
+from db import init_db
+from scraper import scrape_stories
+from manager import get_or_scrape_stories, get_or_scrape_chapters
 from summarizer import summarize_chapter
-from menu import display_story_menu, display_chapter_menu
+from menu import display_main_menu, display_story_menu, display_chapter_menu
 
 def main():
     init_db()
+    model = GPT4All("Meta-Llama-3-8B-Instruct.Q4_0.gguf")
 
     selected_action = display_main_menu()
     while selected_action != 0:
@@ -12,15 +15,15 @@ def main():
             scrape_stories()
 
         # retrieve stories from database
-        stories = get_stories()
+        stories = get_or_scrape_stories()
         selected_story = display_story_menu(stories)
 
         # retrieve chapters from database
-        chapters = get_chapters(selected_story)
+        chapters = get_or_scrape_chapters(selected_story)
         selected_chapter = display_chapter_menu(chapters)
 
         # provide summary of chapter text
-        summary = summarize_chapter(selected_story["id"], selected_chapter["id"])
+        summary = summarize_chapter(model, selected_story, selected_chapter)
 
         print(summary)
 
