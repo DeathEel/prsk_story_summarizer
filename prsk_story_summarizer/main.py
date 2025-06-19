9472e746ef97a2528b5963b19961bf9b4ca29869
deatheel <kevinng2003@gmail.com>
2025-06-19 03:37:14 -0700

    <feat>(main, menu): Redesign menus for better readability. Enhance menus to allow back-navigation.
    Story menu is always extremely long, so menus have been redesigned to show up to 20 items with multiple pages. In addition, back-navigation has been added in the redesign. The main() function was redesigned entirely to support back-navigation.

--- prsk_story_summarizer/main.py
+++ prsk_story_summarizer/main.py
@@ -2,7 +2,7 @@ from db import init_db
 from scraper import scrape_stories
 from manager import get_or_scrape_stories, get_or_scrape_chapters, get_or_scrape_texts
 from summarizer import summarize_chapter
-from menu import display_main_menu, display_story_menu, display_chapter_menu, display_summary_menu
+from menu import display_main_menu, display_story_menu, display_chapter_menu, display_overwrite_menu
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, logging
 
 def main():
@@ -16,45 +16,67 @@ def main():
     tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=cache_dir, use_fast=False)
     model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=cache_dir, torch_dtype="auto", device_map="auto")
 
-    selected_action = display_main_menu()
-    while selected_action != 0:
-        if selected_action == 1:
-            scrape_stories()
+    menu_stack = [("main", lambda: display_main_menu())]
 
-        # retrieve stories from database
-        stories = get_or_scrape_stories()
-        selected_story = display_story_menu(stories)
+    while menu_stack:
+        current_menu_type, current_menu_function = menu_stack[-1]
+        selected_action = current_menu_function()
 
-        # retrieve chapters from database
-        chapters = get_or_scrape_chapters(selected_story)
-        selected_chapters = display_chapter_menu(chapters)
+        # main menu
+        if current_menu_type == "main":
+            if selected_action == "q":
+                menu_stack.pop()
+            elif selected_action != 0:
+                summarize = True if selected_action == 2 else False
+                stories = get_or_scrape_stories()
+                menu_stack.append(("story", lambda: display_story_menu(stories)))
+            else:
+                scrape_stories()
 
-        if (selected_action == 2):
-            for selected_chapter in selected_chapters:
-                # provide entire chapter transcript
-                transcript = get_or_scrape_texts(selected_story, selected_chapter)["transcript"]
-                print(f"\n\tStory: {selected_story["title"]}")
-                print(f"\tChapter: {selected_chapter["title"]}")
-                print(transcript)
-        else:
-            # provide summary of chapter transcript
-            overwrite_desired = display_summary_menu()
-            if overwrite_desired:
-                for selected_chapter in selected_chapters:
-                    summarizer_model = pipeline("text-generation", model=model, tokenizer=tokenizer)
-                    summary = summarize_chapter(summarizer_model, tokenizer, selected_story, selected_chapter, True)
-                    print(f"\n\tStory: {selected_story["title"]}")
-                    print(f"\tChapter: {selected_chapter["title"]}")
-                    print(summary)
+        # story menu
+        elif current_menu_type == "story":
+            if selected_action == "q":
+                menu_stack.pop()
+            else:
+                selected_story = selected_action
+                chapters = get_or_scrape_chapters(selected_story)
+                menu_stack.append(("chapter", lambda: display_chapter_menu(chapters)))
+
+        # chapter menu
+        elif current_menu_type == "chapter":
+            if selected_action == "q":
+                menu_stack.pop()
+                continue
+
+            selected_chapters = selected_action
+
+            if summarize:
+                menu_stack.append(("overwrite", lambda: display_overwrite_menu()))
             else:
                 for selected_chapter in selected_chapters:
+                    transcript = get_or_scrape_texts(selected_story, selected_chapter)["transcript"]
+                    print(f"\n\tStory: {selected_story["title"]}")
+                    print(f"\tChapter: {selected_chapter["title"]}")
+                    print(transcript)
+                    input("\nPress Enter to continue.")
+
+                menu_stack = [("main", lambda: display_main_menu())]
+
+        # overwrite menu
+        elif current_menu_type == "overwrite":
+            if selected_action == "q":
+                menu_stack.pop()
+            else:
+                overwrite = selected_action
+                for selected_chapter in selected_chapters:
                     summarizer_model = pipeline("text-generation", model=model, tokenizer=tokenizer)
-                    summary = summarize_chapter(summarizer_model, tokenizer, selected_story, selected_chapter, False)
+                    summary = summarize_chapter(summarizer_model, tokenizer, selected_story, selected_chapter, overwrite)
                     print(f"\n\tStory: {selected_story["title"]}")
                     print(f"\tChapter: {selected_chapter["title"]}")
                     print(summary)
+                    input("\nPress Enter to continue.")
 
-        selected_action = display_main_menu()
+                menu_stack = [("main", lambda: display_main_menu())]
 
 if __name__ == "__main__":
     main()
--- prsk_story_summarizer/menu.py
+++ prsk_story_summarizer/menu.py
@@ -1,42 +1,85 @@
+import os
+
 def display_main_menu():
-    print("\n0. Quit\n1. Refresh stories\n2. Read stories\n3. Summarize stories")
-    selected_action = get_int_input(f"Select an action to take: ", 0, 3)
+    selected_action = loop_menu("Select an action to take: ", ["Refresh stories", "Read stories", "Summarize stories"])
     return selected_action
 
 def display_story_menu(stories):
-    print("")
-    for idx, story in enumerate(stories):
-        print(f"{idx}: {story['title']}")
-    selected_index = get_int_input(f"Select a story: ", 0, len(stories) - 1)
-    return stories[selected_index]
+    selected_story = loop_menu("Select a story: ", [story["title"] for story in stories])
+    if selected_story == "q":
+        return selected_story
+    return stories[selected_story]
 
 def display_chapter_menu(chapters):
-    print("")
-    for idx, chapter in enumerate(chapters):
-        print(f"{idx}: {chapter['title']}")
-    print(f"{len(chapters)}: All Chapters")
-    selected_index = get_int_input(f"Select a chapter: ", 0, len(chapters))
-
-    if selected_index == len(chapters):
+    selected_chapter = loop_menu("Select a chapter: ", [chapter["title"] for chapter in chapters] + ["All Chapters"])
+    if selected_chapter == "q":
+        return selected_chapter
+    elif selected_chapter == len(chapters):
         return chapters
+    return [chapters[selected_chapter]]
 
-    return [chapters[selected_index]]
-
-def display_summary_menu():
-    print("\n0. No\n1. Yes")
-    selected_action = get_int_input(f"Overwite summary in database?: ", 0, 1)
+def display_overwrite_menu():
+    selected_action = loop_menu("Overwrite summary in database?: ", ["No", "Yes"])
     return selected_action
 
-def get_int_input(prompt, lower_bound, upper_bound):
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def get_valid_input(prompt, lower_bound, upper_bound):
+    user_input = input(prompt)
+    if user_input == "n" or user_input == "p" or user_input == "q":
+        return user_input, ""
+    try:
+        user_input = int(user_input)
+        if lower_bound <= user_input <= upper_bound:
+            return user_input, ""
+        else:
+            return user_input, f"Please enter an integer between {lower_bound} and {upper_bound}, inclusive."
+    except ValueError:
+        return user_input, "Invalid input. Please enter a valid character or integer."
+
+def display_menu(options, start, page_size, error_message):
+    end = min(start + page_size, len(options))
+    true_page_size = end - start    # amount of options displayed in page
+
+    # print blanks to prevent menu from moving
+    if true_page_size < page_size:
+        print("\n" * (page_size - true_page_size - 1))
+
+    # print menu options
+    for i in range(start, end):
+        print(f"{i + 1}. {options[i]}")
+    print("-" * 50)
+    print(f"Showing {start + 1}-{end} of {len(options)}")
+    print("n: next page | p: previous page | q: quit")
+    print(error_message if error_message else "")
+
+    return start, end
+
+def loop_menu(prompt, options):
+    start = 0
+    page_size = 20
+    error = None
+    
     while True:
-        try:
-            user_input = int(input(prompt))
-            if lower_bound <= user_input <= upper_bound:
-                return user_input
-            else:
-                print(f"Please enter an integer between {lower_bound} and {upper_bound}, inclusive.")
-        except ValueError:
-            print("Invalid input. Please enter a valid integer.")
+        clear_screen()
+        start, end = display_menu(options, start, page_size, error)
+        user_input, error = get_valid_input(prompt, start, end)
+
+        if error:
+            continue
+        elif user_input == "n" and start + page_size >= len(options):
+            error = "No next page available."
+        elif user_input == "p" and start - page_size < 0:
+            error = "No previous page available."
+        elif user_input == "q":
+            return user_input
+        elif user_input == "n":
+            start += page_size
+        elif user_input == "p":
+            start -= page_size
+        else:
+            return user_input - 1
 
 # testing
 if __name__ == "__main__":
